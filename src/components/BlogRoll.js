bbdbcb12ebc86593235d50573d1d1ccc78a11b1b
Daniel Berhane <DBERHANE@bmj.com>
2018-05-09 11:21:25 +0100

    editing in the sidebar

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const BlogRoll = () => (
   <div style={{ paddingBottom: '2.5em' }}>
-    <h2>BLOGROLL</h2>
+    <h2>Blogroll</h2>
 
-    <ul>
+    <ul style={{ listStyleType: 'none', paddingBottom: 20 }}>
       <li>ALPSP</li>
       <li>CEBM</li>
       <li>COPE</li>
@@ -15,17 +15,6 @@ const BlogRoll = () => (
       <li>UK PubMed Central Blog</li>
     </ul>
 
-    <ul>
-      <h2>Category</h2>
-
-      <li>BMJ Open editions</li>
-      <li>Events</li>
-      <li>Instructions to authors</li>
-      <li>Open Access</li>
-      <li>Open Peer Review</li>
-      <li>Research Information Network</li>
-      <li>UK PubMed Central Blog</li>
-    </ul>
   </div>
 )
 
