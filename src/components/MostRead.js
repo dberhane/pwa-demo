136ef8f53cf6fa468be9915a7df101c5557061f4
Daniel Berhane <DBERHANE@bmj.com>
2018-05-10 15:21:09 +0100

    ga feed and most read widget updates

--- src/components/MostRead.js
+++ src/components/MostRead.js
@@ -4,14 +4,14 @@ import Link from 'gatsby-link'
 const MostRead = ({ gdata, ghead = 'Most Read', gsite = 0 }) => {
   const mostReadItems = gdata[gsite].node.bmjsiteData
   const websiteUrl = gdata[gsite].node.bmjsiteHead[0].websiteUrl
-
+console.log(mostReadItems)
   return (
     <div>
       <h2>{ghead}</h2>
       <ol style={{ paddingBottom: 20 }}>
         {mostReadItems.map((node, i = 0) => (
           <li style={{ paddingBottom: 10 }} key={i++}>
-            <Link to={node.pagePath}>{node.pageTitle}</Link>
+            <a href={`${websiteUrl}${node.pagePath}`}>{node.pageTitle}</a>
           </li>
         ))}
       </ol>
